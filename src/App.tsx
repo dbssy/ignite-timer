18017a655c9bf1b6b8008dce2ea43c6d11667e56
João Debussy <social.debussy@gmail.com>
2022-10-11 20:07:36 -0300

    feat: implementing react-router-dom and Home and History pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
+import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
 
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/global'
 
+import { Router } from './Router'
+
 export function App() {
-	return (
-		<ThemeProvider theme={defaultTheme}>
-			<GlobalStyle />
-		</ThemeProvider>
-	)
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </ThemeProvider>
+  )
 }
