6975fb245de2b1965edb2eaea16da76ba9579aa9
João Debussy <social.debussy@gmail.com>
2022-10-11 21:32:58 -0300

    style: add Status component

--- src/pages/History/styles.ts
+++ src/pages/History/styles.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import styled from 'styled-components'
 
 export const HistoryContainer = styled.main`
@@ -57,5 +58,30 @@ export const HistoryList = styled.div`
         padding-right: 1.5rem;
       }
     }
+  }
+`
+
+const STATUS_COLORS = {
+  yellow: 'yellow-500',
+  green: 'green-500',
+  red: 'red-500',
+} as const
+
+interface StatusProps {
+  statusColor: keyof typeof STATUS_COLORS;
+}
+
+export const Status = styled.span<StatusProps>`
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+
+  &::before {
+    content: '';
+    width: 0.5rem;
+    height: 0.5rem;
+    border-radius: 50%;
+    background: ${({ theme, statusColor }) =>
+    theme[STATUS_COLORS[statusColor]]};
   }
 `
